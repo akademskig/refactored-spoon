96eee3598e74f5de5f6807e0caad0afbf86a75cc
marta <m.susek@live.com>
2025-03-29 14:45:25 +0100

    client: finish lastNewsWidget

--- client/src/components/LatestNewsWidget.tsx
+++ client/src/components/LatestNewsWidget.tsx
@@ -1,25 +1,78 @@
+import { useEffect, useRef, useState } from 'react';
+import api from '../services/api';
 import styles from '../styles/latestNewsWidget.module.scss';
 
+type Article = {
+  id: string;
+  title: string;
+  published_date: string;
+};
+
+const title = 'Latest news';
+const description = 'See all news';
+
 const LatestNewsWidget = () => {
-  const latest = [
-    { time: '14:30', title: 'How To Write Better Advertising Copy' },
-    { time: '14:30', title: '6 Powerful Tips To Create Testimonials...' },
-    // ...
-  ];
+  const [latest, setLatest] = useState<Article[]>([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const observerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const res = await api.get('/articles', {
+          params: { page },
+        });
+
+        if (res.data.length === 0) setHasMore(false);
+        else setLatest((prev) => [...prev, ...res.data]);
+      } catch (err) {
+        console.error('Failed to fetch latest news', err);
+      }
+    };
+    fetch();
+  }, [page]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPage((prev) => prev + 1);
+        }
+      },
+      { threshold: 1.0 },
+    );
+
+    if (observerRef.current) observer.observe(observerRef.current);
+    return () => observer.disconnect();
+  }, [hasMore]);
 
   return (
     <div className={styles.root}>
       <h3>
-        <span className={styles.dot} /> Latest news
+        <span className={styles.dotOuter}>
+          <span className={styles.dotInner} />
+        </span>
+        {title}
       </h3>
-      <ul>
-        {latest.map((n, i) => (
-          <li key={i}>
-            <span>{n.time}</span>
+      <ul className={styles.scrollArea}>
+        {latest.map((n) => (
+          <li key={n.id}>
+            <span>
+              {new Date(n.published_date).toLocaleTimeString([], {
+                hour: '2-digit',
+                minute: '2-digit',
+              })}
+            </span>
             <p>{n.title}</p>
           </li>
         ))}
+        <div ref={observerRef} className={styles.scrollTrigger} />
       </ul>
+
+      <div className={styles.footer}>
+        <a href="#">{description} &rarr;</a>
+      </div>
     </div>
   );
 };
