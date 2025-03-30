a80b55983ac70fd990a2965a190be284784d6a03
marta <m.susek@live.com>
2025-03-30 13:59:29 +0200

    client: remove signIn and signUp pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import './styles/main.scss';
 import Layout from './components/Layout';
@@ -13,8 +11,6 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="favorites" element={<Favorites />} />
       </Route>
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
