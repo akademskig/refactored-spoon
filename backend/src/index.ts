093385499fe92fca5597ab2b7911c72f485e4eee
marta <m.susek@live.com>
2025-03-31 11:58:12 +0200

    dev/backend: add mongo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import routes from './routes';
+import { connectMongo } from './db/connectMongo';
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173', 
+    credentials: true,
+  }),
+);
 app.use(express.json());
+connectMongo();
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 5000;
