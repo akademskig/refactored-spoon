81cda93b2e30e63ee7718ec1076359e599c91122
marta <m.susek@live.com>
2025-04-01 12:23:19 +0200

    dev/client: more mobile adjustments

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { Home, Newspaper, BookMarked } from 'lucide-react';
 import styles from './Sidebar.module.scss';
-import { NavLink, useLocation, useParams } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { SIDEBAR_ITEMS } from './sidebarItems';
 import { useAuth } from '../../hooks/useAuth';
 import { ICONS } from './icons';
@@ -11,33 +11,35 @@ type Props = {
 };
 
 const Sidebar = ({ categories }: Props) => {
-  const { slug } = useParams();
-  const { pathname } = useLocation();
   const { user } = useAuth();
 
   return (
     <aside className={styles.sidebar}>
       <nav>
-        <NavLink to={SIDEBAR_ITEMS.home.route}>
-          <button className={pathname === SIDEBAR_ITEMS.home.route ? styles.active : ''}>
-            <Home />
-            {capitalize(SIDEBAR_ITEMS.home.name)}
-          </button>
+        <NavLink
+          to={SIDEBAR_ITEMS.home.route}
+          className={({ isActive }) => `${styles.item} ${isActive ? styles.active : ''}`}
+        >
+          <Home />
+          {capitalize(SIDEBAR_ITEMS.home.name)}
         </NavLink>
         {categories.map((c) => (
-          <NavLink to={`category/${c}`} key={c}>
-            <button key={c} className={slug === c ? styles.active : ''}>
-              {ICONS[c] || <Newspaper />}
-              {capitalize(c)}
-            </button>
+          <NavLink
+            to={`category/${c}`}
+            key={c}
+            className={({ isActive }) => `${styles.item} ${isActive ? styles.active : ''}`}
+          >
+            {ICONS[c] || <Newspaper />}
+            {capitalize(c)}
           </NavLink>
         ))}
         {user && (
-          <NavLink to={SIDEBAR_ITEMS.favorites.route}>
-            <button className={pathname === SIDEBAR_ITEMS.favorites.route ? styles.active : ''}>
-              <BookMarked />
-              {capitalize(SIDEBAR_ITEMS.favorites.name)}
-            </button>
+          <NavLink
+            to={SIDEBAR_ITEMS.favorites.route}
+            className={({ isActive }) => `${styles.item} ${isActive ? styles.active : ''}`}
+          >
+            <BookMarked />
+            {capitalize(SIDEBAR_ITEMS.favorites.name)}
           </NavLink>
         )}
       </nav>
